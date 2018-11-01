8d91ef5b78705322e1607708ca083794d827b00d
Aditya Tekumalla <tekumallaaditya@gmail.com>
2018-11-01 21:17:11 +0100

    corrections

--- entabulature_py3/settings.py
+++ entabulature_py3/settings.py
@@ -150,8 +150,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-""" PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static') """
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'assets'),
