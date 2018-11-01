b19e7df7c6ce2adda8dcf463939873ca1c07ac56
Aditya Tekumalla <tekumallaaditya@gmail.com>
2018-11-01 12:42:36 +0100

    trying sqlite3

--- entabulature_py3/settings.py
+++ entabulature_py3/settings.py
@@ -75,19 +75,19 @@ WSGI_APPLICATION = 'entabulature_py3.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'djongo',
-        'NAME': 'Django_Entabulature',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'djongo',
+#         'NAME': 'Django_Entabulature',
+#     }
+# }
 
 
 # Password validation
