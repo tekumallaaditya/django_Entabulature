08df2de44a83d7269afbfc04a54845b9c6e51147
Aditya Tekumalla <tekumallaaditya@gmail.com>
2018-11-01 13:00:17 +0100

    corrections

--- entabulature_py3/settings.py
+++ entabulature_py3/settings.py
@@ -141,7 +141,7 @@ DATABASES['default'].update(db_from_env)
 STATIC_URL = '/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'assets'),
 # )
--- entabulature_py3/wsgi.py
+++ entabulature_py3/wsgi.py
@@ -14,6 +14,8 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'entabulature_py3.settings')
 
 application = get_wsgi_application()
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
 
 
 
